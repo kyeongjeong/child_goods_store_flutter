9c016d907d408541c694f31dbf0eed481a0fc24e
wjlee611 <wjlee611@gmail.com>
2024-02-25 14:54:21 +0900

    Feat: "Add Config, Constants"
    Flavor와 연동될 Config 파일과
    앱 내부에서 사용될 상수들을 정의했습니다.

---
+++ lib/constants/strings.dart
@@ -0,0 +1,10 @@
+class Strings {
+  /// EMPTY STRING
+  static const String nullStr = '';
+
+  /// JWT TOKEN KEY
+  static const String jwtToken = 'JWT_TOKEN';
+
+  /// UNKNOWN ERROR MESSAGE
+  static const String unknownFail = '요청 처리에 실패했습니다.';
+}
