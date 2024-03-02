53afcad96b785e0847185d42633455793d6d1891
wjlee611 <wjlee611@gmail.com>
2024-03-02 21:44:41 +0900

    Chore: "add auth & firebase package"
    사용자 인증과 Firebase에 필요한 라이브러리를 적용했습니다.
    
    그 과정에서 firebase의 flavor 설정도 완료되었습니다.
    보안과 관련된 파일의 목록은 .gitignore 파일의 `# Secret` 하단을 참고해주세요.
    
    설정 과정에서 iOS 번들명이 snake_case가 아닌 camelCase로 작성되어야 했기에
    번들명이 변경되었습니다.

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,20 @@
+// Import from dev dependency
+import 'package:child_goods_store_flutter/configs/firebase_options_dev.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'flavors.dart';
 
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.dev;
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await runner.main();
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,8 +1,20 @@
+// Import from prod dependency
+import 'package:child_goods_store_flutter/configs/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'flavors.dart';
 
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.prod;
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await runner.main();
 }
